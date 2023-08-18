08a45fbb2692b08553898340e65ba33b141cca04
Thiago Alves <80608138+Thiagof2755@users.noreply.github.com>
2023-08-18 13:53:43 -0300

    ini

--- Taskify/routes/web.php
+++ Taskify/routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\DB;
-use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,51 +15,4 @@ use App\Models\User;
 
 Route::get('/', function () {
     return view('welcome');
-});
-Route::get('/testar-conexao', function () {
-    try {
-        DB::connection()->getPdo();
-        return 'Conexão bem-sucedida!';
-    } catch (\Exception $e) {
-        return 'Erro ao conectar ao banco de dados: ' . $e->getMessage();
-    }
-});
-Route::get('/testar-tabelas', function () {
-    try {
-        $tables = DB::select('SHOW TABLES');
-        $tableNames = [];
-
-        foreach ($tables as $table) {
-            $tableName = reset($table);
-            $tableNames[] = $tableName;
-        }
-
-        return $tableNames;
-    } catch (\Exception $e) {
-        return 'Erro ao obter as tabelas do banco de dados: ' . $e->getMessage();
-    }
-});
-
-Route::get('/inserir-usuario', function () {
-    try {
-        $user = new User();
-        $user->name = 'John Doe';
-        $user->email = 'john@example.com';
-        $user->password = bcrypt('secret');
-        $user->save();
-        
-        return 'Usuário inserido com sucesso!';
-    } catch (\Exception $e) {
-        return 'Erro ao inserir usuário: ' . $e->getMessage();
-    }
-});
-Route::get('/remover-usuario/{id}', function ($id) {
-    try {
-        $user = User::findOrFail($id);
-        $user->delete();
-
-        return 'Usuário removido com sucesso!';
-    } catch (\Exception $e) {
-        return 'Erro ao remover usuário: ' . $e->getMessage();
-    }
 });
